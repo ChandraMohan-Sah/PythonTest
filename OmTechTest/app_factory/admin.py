9bcb5ef55f9c95e59ea026d98a0fb36f15529ac4
ChandraMohan-Sah <csah9628@gmail.com>
2025-07-23 12:47:35 +0545

    feat: Made Database Schema

--- OmTechTest/app_factory/admin.py
+++ OmTechTest/app_factory/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CementFactoryInfo, NewsSectionInfo, PriceHistoryInfo
 
 # Register your models here.
+admin.site.register(CementFactoryInfo)
+admin.site.register(NewsSectionInfo)
+admin.site.register(PriceHistoryInfo)
--- OmTechTest/app_factory/models.py
+++ OmTechTest/app_factory/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class CementFactoryInfo(models.Model):
+    company_name = models.CharField(max_length=255)
+    sector = models.CharField(max_length=100)
+    email = models.EmailField()
+    paid_up = models.IntegerField(help_text="Paid-up capital in rupees")
+
+    def __str__(self):
+        return f"Factory Information: {self.company_name}"
+
+
+class NewsSectionInfo(models.Model):
+    company = models.ForeignKey(CementFactoryInfo, on_delete=models.CASCADE, related_name='news')
+    news_title = models.CharField(max_length=255)
+    news_date = models.DateTimeField()
+    news_image = models.ImageField(upload_to='news_images/')
+    news_body = models.TextField()
+
+    def __str__(self):
+        return f"News Title: {self.news_title}"
+
+
+class PriceHistoryInfo(models.Model):
+    company = models.ForeignKey(CementFactoryInfo, on_delete=models.CASCADE, related_name='price_history')
+    date = models.DateTimeField()
+    open_price = models.FloatField()
+    high_price = models.FloatField()
+    low_price = models.FloatField()
+    close_price = models.FloatField()
+
+    def __str__(self):
+        return f"Price History on {self.date.strftime('%Y-%m-%d')} for {self.company.company_name}"
